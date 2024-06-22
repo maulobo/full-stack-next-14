ba0f627c2f57656c7464adfa8ec6fdf9e1bea19a
Mauro <maurolobo.ml@gmail.com>
2024-06-22 00:55:08 -0300

    slint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
-module.exports = {
-  // Otros ajustes de ESLint
-  plugins: ["typescript"],
-  rules: {
-    // Otras reglas de ESLint
-    "typescript/no-explicit-any": "off", // Esto es un ejemplo, ajusta según tus necesidades
-    "typescript/consistent-type-assertions": [
-      "error",
-      { assertionStyle: "as", objectLiteralTypeAssertions: "allow" },
-    ],
-  },
-};
+// module.exports = {
+//   // Otros ajustes de ESLint
+//   plugins: ["typescript"],
+//   rules: {
+//     // Otras reglas de ESLint
+//     "typescript/no-explicit-any": "off", // Esto es un ejemplo, ajusta según tus necesidades
+//     "typescript/consistent-type-assertions": [
+//       "error",
+//       { assertionStyle: "as", objectLiteralTypeAssertions: "allow" },
+//     ],
+//   },
+// };
